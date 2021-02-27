ca8c38194dfa2e972fcc261ec26ccbdb8a76c90e
PYJS123 <73827861+PYJS123@users.noreply.github.com>
2021-02-27 12:34:54 +0000

    Basic dino

--- dino.js
+++ dino.js
@@ -1,10 +1,17 @@
 class Dino {
-  constructor(x, y, colour) {
+  constructor(x, y, w, h, colour) {
     this.x = x;
     this.y = y;
+    this.w = w;
+    this.h = h;
     this.colour = colour;
 
     this.gravity = 1;
     this.acc = 1;
+  }
+
+  show() {
+    fill(this.colour);
+    rect(this.x, this.y, this.w, this.h);
   }
 }
