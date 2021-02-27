882fa37c6ec336134c3abae0247219624bbfb6fe
PYJS123 <73827861+PYJS123@users.noreply.github.com>
2021-02-27 12:34:28 +0000

    Accommodating for a new Dino

--- sketch.js
+++ sketch.js
@@ -4,16 +4,22 @@ let ground = {
   thickness: 10
 };
 
+let dino;
+
 function setup() {
   createCanvas(850, 400);
   ground.level = height - 50;
   ground.colour = color(28, 28, 28);
+
+  dino = new Dino(50, ground.level - 90, 40, 90, color(51, 51, 51));
 }
 
 function draw() {
   background(100);
 
   drawGround();   // Draw the ground
+
+  dino.show();
 }
 
 function drawGround() {
