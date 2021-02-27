4e17080dab81d9e72f08c361b97c8d4081429d47
PYJS123 <73827861+PYJS123@users.noreply.github.com>
2021-02-27 12:22:43 +0000

    Draw ground

--- sketch.js
+++ sketch.js
@@ -1,7 +1,23 @@
+let ground = {
+  colour: null,
+  level: null,
+  thickness: 10
+};
+
 function setup() {
   createCanvas(850, 400);
+  ground.level = height - 50;
+  ground.colour = color(28, 28, 28);
 }
 
 function draw() {
   background(100);
+
+  drawGround();   // Draw the ground
+}
+
+function drawGround() {
+  stroke(ground.colour);
+  strokeWeight(ground.thickness);
+  line(0, ground.level, width, ground.level);
 }
